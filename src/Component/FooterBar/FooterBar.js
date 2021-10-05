3266e42e6ab7ee723200a8498ab2445c1fd26d1b
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-10-05 09:42:01 +0600

    add some change on navbar and footer

--- src/Component/FooterBar/FooterBar.js
+++ src/Component/FooterBar/FooterBar.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div>
             <footer>
-                <h6 className='bg-dark text-light p-5 mt-5'> Copyright 1999-2021 by Learn your self. All Rights Reserved LearnYourSelf is Powered by LearnYourSelf</h6>
+                <h6 className='bg-dark text-light p-5 mt-5'> &copy; Copyright 1999-2021 by Happy Learning. All Rights Reserved Happy Learning is Powered by Happy Learning</h6>
             </footer>
         </div>
     );
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
     return (
         <div>
            <Navbar bg="dark" variant="dark">
-                <Container>    
+                <Container>
+                <Link class="navbar-brand me-5 fs-3" to="#">Happy Learning</Link>    
                     <Nav className="me-auto">
                         <Link className='text-light text-decoration-none me-3' to="/home">Home</Link>
                         <Link className='text-light text-decoration-none me-3' to="/courses">Courses</Link>
