bb62bf3cda09d2c0438bc9a3c97e85248d5a782b
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-10-05 21:55:48 +0600

    last work on not found page

--- src/Component/NotFound/NotFound.js
+++ src/Component/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 const NotFound = (props) => {
+    const imgUrl=`https://thumbs.dreamstime.com/b/error-page-not-found-glitch-vector-illustration-error-page-not-found-glitch-vector-illustration-114821200.jpg`;
     return (
-        <div>
-            <h1>Not found</h1>
+        <div className=''>
+            <Image className='mt-5' src={imgUrl} fluid rounded />
         </div>
     );
 };
